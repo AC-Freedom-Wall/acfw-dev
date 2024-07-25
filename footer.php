2259764e98d551b084fbc2c6c96ba5e8fe27efc5
contactjs078 <contactjs078@gmail.com>
2024-07-25 13:11:03 -0400

    <span>Algonquin College Freedom Wall</span> <br>     <span><em>say what you want to say</em></span> <br>     <span>&copy; 2024</span>

--- footer.php
+++ footer.php
@@ -1,5 +1,7 @@
 <footer>
-    <span>&copy; 2024 My Website</span>
+    <span>Algonquin College Freedom Wall</span> <br>
+    <span><em>say what you want to say</em></span> <br>
+    <span>&copy; 2024</span>
 </footer>
 
 </body>
