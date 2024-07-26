7229ed7b9652a8b7f361db43f6978b395f447b0f
contactjs078 <contactjs078@gmail.com>
2024-07-26 13:35:18 -0400

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,6 +1,10 @@
 <footer>
-    <span>Algonquin College Freedom Wall</span> <br>
-    <span><em>say what you want to say</em></span> <br>
+<span>Algonquin College Freedom Wall</span><br>
+    <span><em>say what you want to say</em></span><br>
+    <nav>
+      <a href="http://dev.acfw.com/aboutus.php">About Us</a>
+      <a href="#">Contact Us</a>
+    </nav>
     <span>&copy; 2024</span>
 </footer>
 
