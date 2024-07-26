3ab07e1c668708acbb24b128d8b25b7edbf19a21
contactjs078 <contactjs078@gmail.com>
2024-07-26 13:04:13 -0400

    about us page

---
+++ aboutus.php
@@ -0,0 +1,29 @@
+<?php
+include $_SERVER['DOCUMENT_ROOT']."/head.php";
+include $_SERVER['DOCUMENT_ROOT']."/header.php";
+?>
+
+<div class="aboutus">
+
+    <h2>About Us: the Algonquin College Freedom Wall</h2>
+    <h3><strong>Your Voice, Your Space</strong></p>
+    <p>The Algonquin College Freedom Wall is a digital platform designed to foster open dialogue, creativity, and community among our students. Here, you have the freedom to express your thoughts, ideas, and feelings without fear of judgment or reprisal.</p>
+    
+    <img src="img/studentsspeakout.png" alt="students speakout image">
+    
+    <h3><strong>A Safe Haven for Expression</strong></p>
+    <p>We believe that every student has a unique perspective to share. The Freedom Wall is a space where you can:</p>
+    <ul class="a">
+        <li>Share your thoughts freely: Post anonymously about anything that's on your mind.</li>
+        <li>Connect with others: Engage in anonymous discussions and build community.</li>
+        <li>Inspire and be inspired: Discover new ideas and perspectives from your peers.</li>
+    </ul>
+    <p>We are committed to providing a safe and inclusive environment for all students. Please be respectful of others and adhere to our community guidelines.</p>
+    <h3><strong>Let your voice be heard. Join the conversation today!</strong></p>
+    <img src="img/speakout.jpg" alt="speech bubble">
+
+</div>
+<?php
+include $_SERVER['DOCUMENT_ROOT']."/footer.php";
+?>
+
