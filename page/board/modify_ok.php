5a87b69691a3d3d5f5ec27c6f53e8e1614895f5e
haebin yoon <yoon0032@algonquinlive.com>
2024-07-27 16:48:49 -0400

    Update modify_ok.php to fix meta refresh URL and add comment

--- page/board/modify_ok.php
+++ page/board/modify_ok.php
@@ -10,4 +10,5 @@ $content = addslashes($_POST['content']);
 $sql = query("update board set name='".$username."',pw='".$userpw."',title='".$title.")',content='".$content."' where idx='".$bno."'"); ?>
 
 <script type="text/javascript">alert("Fixed."); </script>
-<meta http-equiv="refresh" content="0 url=/page/board/read.php?idx=<?php echo $bno; ?>">
+<meta http-equiv="refresh" content="0 url=/"/>
+<!-- <meta http-equiv="refresh" content="0 url=/page/board/read.php?idx=<?php echo $bno; ?>"> -->
