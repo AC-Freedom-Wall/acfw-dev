4f9ef15c34a33cd58edbbbe9b081de27f731ef5a
contactjs078 <contactjs078@gmail.com>
2024-07-27 15:29:34 -0400

    Update join_form.php

--- page/member/join_form.php
+++ page/member/join_form.php
@@ -1,35 +1,48 @@
 <?php 
 include $_SERVER['DOCUMENT_ROOT']."/head.php";
 include $_SERVER['DOCUMENT_ROOT']."/header.php";
+
 if(isset($_SESSION['userid'])){
-	echo "<script>alert('Incorrect approach.'); history.back();</script>"; 
-}else{
+    echo "<script>alert('Incorrect approach.'); history.back();</script>"; 
+} else {
 ?>
-	<div id="join_form_in">
-		<h2>Sign up</h2>
-			<form action="join_ok.php" method="post">
-				<div id="join_f">
-					<div class="form-group">
-						<label for="userid">ID</label>
-						<div class="mb"><input type="text" class="inp" id="userid" name="userid" placeholder="ID" /></div>
-					</div>
-					<div class="form-group">
-						<label for="userpw">Password</label>
-						<div class="mb"><input type="password" class="inp" id="userpw" name="userpw" placeholder="Password" /></div>
-					</div>
-					<div class="form-group">
-						<label for="name">Name</label>
-						<div class="mb"><input type="text" class="inp" id="name" name="name" placeholder="Please enter a name" /></div>
-					</div>
-				    <div class="form_btn">
-				    	<button type="submit" class="form_bt">submit</button>
-				       	<button type="reset" class="form_bt2">reset</button>
-				    </div>
-				</div> <!-- join_f end -->
-			</form>
-		</div>
+    <div id="join_form_in">
+        <h2>Sign up</h2>
+        <form id="signupForm" action="join_ok.php" method="post" onsubmit="return validateForm()">
+            <div id="join_f">
+                <div class="form-group">
+                    <label for="userid">ID</label>
+                    <div class="mb"><input type="text" class="inp" id="userid" name="userid" placeholder="ID" /></div>
+                    <span class="error" id="useridError"></span>
+                </div>
+                <div class="form-group">
+                    <label for="userpw">Password</label>
+                    <div class="mb"><input type="password" class="inp" id="userpw" name="userpw" placeholder="Password" /></div>
+                    <span class="error" id="userpwError"></span>
+                </div>
+                <div class="form-group">
+                    <label for="retypepw">Retype Password</label>
+                    <div class="mb"><input type="password" class="inp" id="retypepw" name="retypepw" placeholder="Retype Password" /></div>
+                    <span class="error" id="retypepwError"></span>
+                </div>
+                <div class="form-group">
+                    <label for="email">Email</label>
+                    <div class="mb"><input type="email" class="inp" id="email" name="email" placeholder="Email" /></div>
+                    <span class="error" id="emailError"></span>
+                </div>
+                <div class="form-group">
+                    <label for="name">Name</label>
+                    <div class="mb"><input type="text" class="inp" id="name" name="name" placeholder="Please enter a name" /></div>
+                    <span class="error" id="nameError"></span>
+                </div>
+                <div class="form_btn">
+                    <button type="submit" class="form_bt">Submit</button>
+                    <button type="reset" class="form_bt2">Reset</button>
+                </div>
+            </div> <!-- join_f end -->
+        </form>
+    </div>
 <?php
-} 
+}
 include $_SERVER['DOCUMENT_ROOT']."/footer.php";
 ?>
-
