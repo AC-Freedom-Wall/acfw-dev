b34b22cc15b44c308f7385b4407e19f6818a74a1
haebin yoon <yoon0032@algonquinlive.com>
2024-07-27 20:25:53 -0400

    rollback to the previous commit

--- page/board/content.php
+++ page/board/content.php
@@ -146,8 +146,9 @@ if ($result->num_rows > 0) {
 				if(isset($_SESSION['userid'])){
 			?>
 
-				<li><button class="moddelbutton" href="#">Modify</a></li>
-				<li><button class="moddelbutton" href="#">Delete</a></li>
+				<li><button class="dat_edit_bt" href="#">Modify</a><li>
+				<li><button class="dat_delete_bt" href="#">Delete</a><li>
+
 			<?php
 				}
 			?>
