6f19963c9f5a5af977d1ac0e1133aa91933f041a
contactjs078 <contactjs078@gmail.com>
2024-07-29 16:36:00 -0400

    Update contactus.php

--- contactus.php
+++ contactus.php
@@ -4,22 +4,22 @@ include $_SERVER['DOCUMENT_ROOT']."/header.php";
 ?>
 
 <div class="contactus">
-    <h2>Contact Us</h2>
+    <h2>Contact Us</h2><br>
     <form id="contactForm" action="" method="GET">
         <div class="form-group">
             <label for="namecs">Pseudonym:</label>
             <input type="text" id="namecs" name="namecs" placeholder="Stay anonymous. Just use as pseudonym.">
-            <span class="error" id="nameError"></span>
+            <span class="error" id="nameError"></span><br>
         </div>
         <div class="form-group">
             <label for="emailcs">Email:</label>
             <input type="email" id="emailcs" name="emailcs">
-            <span class="error" id="emailError"></span>
+            <span class="error" id="emailError"></span><br>
         </div>
         <div class="form-group">
             <label for="message">Message:</label>
             <textarea id="message" name="message"></textarea>
-            <span class="error" id="messageError"></span>
+            <span class="error" id="messageError"></span><br>
         </div>
         <button type="submit">Submit</button>
     </form>
